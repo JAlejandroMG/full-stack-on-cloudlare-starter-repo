48c2292ff82756fee416e46b359d3905d0fc533a
JAlejandroMG <jamg-developer@tuta.com>
2025-12-04 13:33:04 -0600

    feat(feature): ✨ 06-CloudflareWorkerLocationHeaders
    Get the Cloudflare Worker header information to locate the request origin geographically.

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -6,7 +6,19 @@ import { Hono } from 'hono';
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
+	//~ Hono make some preprocess to the original Cloudflare request
+	//~ so this request is not the same as the one in src/index.ts
+	//~ Hono attaches a few different helper methods to make
+	//~ working with like cookies and headers really simple
+	// console.log(JSON.stringify(c.req.raw.cf));
+	const cf = c.req.raw.cf;
+	const country = cf?.country;
+	const lat = cf?.latitude;
+	const long = cf?.longitude;
+
 	return c.json({
-		message: 'Hello world',
+		country,
+		lat,
+		long,
 	});
 });
